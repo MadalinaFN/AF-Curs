857ca924eb6da3a875a170e7f4446a91a5dd555d
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-03-02 09:23:32 +0200

    Sortare

--- Curs2/Program.cs
+++ Curs2/Program.cs
@@ -12,7 +12,41 @@ namespace Curs2
         {
             //kvalori();
             //CifreComune();
-            CelMaiMareSiCelMaiMic();
+            //CelMaiMareSiCelMaiMic();
+            //Sortare();
+        }
+
+        private static void Sortare()
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            int[] v = new int[n];
+            int[] vn = new int[1001];
+
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = rnd.Next(1001);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                vn[v[i]]++;
+            }
+
+            for (int i = 0; i < 1001; i++)
+            {
+                Console.Write($"{vn[i]} ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 1001; i++)
+            {
+                for (int j = 0; j < vn[i]; j++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
         }
 
         private static void CelMaiMareSiCelMaiMic()
