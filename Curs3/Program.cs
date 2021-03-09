54d433060b89ce655cb274f37bc22c3019ddd82f
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-03-09 08:22:49 +0200

    MatricePatratica

---
+++ Curs3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MatricePatratica();
+
+        }
+
+        private static void MatricePatratica()
+        {
+            int n = 5;
+            int[,] a = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i < j) && (i + j < n - 1))
+                    {
+                        a[i, j] = 1;
+                    }
+                    if ((i < j) && (i + j > n - 1))
+                    {
+                        a[i, j] = 2;
+                    }
+                    if ((i > j) && (i + j < n - 1))
+                    {
+                        a[i, j] = 3;
+                    }
+                    if ((i > j) && (i + j > n - 1))
+                    {
+                        a[i, j] = 4;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{a[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
