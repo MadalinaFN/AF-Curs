8feca885861b32f64e814c54c93e2a2316ccf914
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-03-09 08:39:27 +0200

    PatratConcentric

--- Curs3/Program.cs
+++ Curs3/Program.cs
@@ -11,7 +11,54 @@ namespace Curs3
         static void Main(string[] args)
         {
             //MatricePatratica();
+            //PatratConcentric();
+        }
 
+        static Random rnd = new Random();
+
+        private static void PatratConcentric()
+        {
+            int n = 8, k = 1, m = 7;
+            int[,] a = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    a[i, j] = k;
+                    k++;
+                    if (k == 10)
+                    {
+                        k = 1;
+                    }
+                }
+            }
+            for (int i = 0; i < m - 1; i++)
+            {
+                Console.Write(a[0,i] + " ");
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                Console.Write(a[i, m - 1] + " ");
+            }
+            for (int i = m - 1; i >= 0; i--)
+            {
+                Console.Write(a[n - 1, i] + " ");
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(a[i, 0] + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{a[i, j]} ");
+                }
+                Console.WriteLine();
+            }
         }
 
         private static void MatricePatratica()
