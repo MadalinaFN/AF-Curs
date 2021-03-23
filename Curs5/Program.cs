d98901d0395185d86112a50042a65032a4968ba2
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-03-23 08:29:16 +0200

    Fibonacci Recursiv

---
+++ Curs5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci();
+        }
+
+        private static void Fibonacci()
+        {
+            Console.WriteLine(fib(6));
+        }
+
+        static int fib(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
