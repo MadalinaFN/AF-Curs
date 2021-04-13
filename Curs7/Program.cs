cab95a549f02e43c0ad764a17f0046f57e13f682
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-04-13 08:33:31 +0300

    Permutari

--- Curs7/Program.cs
+++ Curs7/Program.cs
@@ -33,11 +33,78 @@ namespace Curs7
             }
         }
 
+        static void back1(int k, int n, int[] sol)
+        {
+            if (k >= n)
+            {
+                bool ok = true;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (sol[i] == sol[j])
+                        {
+                            ok = false;
+                            break;
+                        }
+                    }
+                }
+                if (ok)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.Write(sol[i]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    sol[k] = i + 1;
+                    back1(k + 1, n, sol);
+                }
+            }
+        }
+
+        static void permutari(int k, int n, int[] sol, bool[] vis)
+        {
+            if (k >= n)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(sol[i]);
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (!vis[i])
+                    {
+                        vis[i] = true;
+                        sol[k] = i + 1;
+                        permutari(k + 1, n, sol, vis);
+                        vis[i] = false;
+                    }
+                }
+            }
+        }
+
         private static void Backtracking()
         {
             int n = int.Parse(Console.ReadLine());
             int[] v = new int[n];
-            back(0, n, v);
+            bool[] vis = new bool[n];
+            //back(0, n, v);
+            //back1(0, n, v);
+            for (int i = 0; i < n; i++)
+            {
+                vis[i] = false;
+            }
+            permutari(0, n, v, vis);
         }
     }
 }
