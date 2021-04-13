f2a20e5cf1d7342f8a48aa79ec8ccf9de7f067c4
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-04-13 08:17:46 +0300

    Backtracking

---
+++ Curs7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Backtracking();
+        }
+
+        static void back(int k, int n, int[] sol)
+        {
+            if (k >= n)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(sol[i]);
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    sol[k] = i;
+                    back(k + 1, n, sol);
+                }
+            }
+        }
+
+        private static void Backtracking()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            back(0, n, v);
+        }
+    }
+}
