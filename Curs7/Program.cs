5198ed22a6c51166ed1097faf452cdc8142bef5a
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-04-13 09:33:48 +0300

    ProblemaMatrice

--- Curs7/Program.cs
+++ Curs7/Program.cs
@@ -10,7 +10,26 @@ namespace Curs7
     {
         static void Main(string[] args)
         {
-            Backtracking();
+            //Backtracking();
+            ProblemaMatrice();
+        }
+
+        private static void ProblemaMatrice()
+        {
+            int interc = 0;
+            init();
+            do
+            {
+                interchange();
+                interc++;
+            } while (!verify());
+            view();
+            Console.WriteLine(interc);
+            Console.WriteLine();
+
+            int[] v = new int[9];
+            bool[] vis = new bool[9];
+            permutari_pmag(0, 9, v, vis);
         }
 
         static void back(int k, int n, int[] sol)
@@ -159,7 +178,7 @@ namespace Curs7
         {
             if (k > p)
             {
-                for (int i = 0; i < p; i++)
+                for (int i = 1; i <= p; i++)
                 {
                     Console.Write(sol[i]);
                 }
@@ -169,9 +188,101 @@ namespace Curs7
             {
                 for (int i = sol[k - 1] + 1; i <= n; i++)
                 {
-
                     sol[k] = i;
                     combinari1(k + 1, n, p, sol);
+                }
+            }
+        }
+
+        //Se da o matrice de 3x3 cu valori distincte in multimea[1...9]. O solutie
+        //presupune suma pe fiecare linie sa fie egala cu suma pe fiecare coloana si
+        //cu suma pe fiecare diagonala. Afisati o solutie daca exista.
+        static int[,] m = new int[3, 3];
+        static Random rnd = new Random();
+        static void init()
+        {
+            int k = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    m[i, j] = k;
+                    k++;
+                }
+            }
+        }
+        static void interchange()
+        {
+            int l1 = rnd.Next(3);
+            int c1 = rnd.Next(3);
+            int l2 = rnd.Next(3);
+            int c2 = rnd.Next(3);
+
+            int aux = m[l1, c1];
+            m[l1, c1] = m[l2, c2];
+            m[l2, c2] = aux;
+        }
+        static bool verify()
+        {
+            int sl1 = m[0, 0] + m[0, 1] + m[0, 2];
+            int sl2 = m[1, 0] + m[1, 1] + m[1, 2];
+            int sl3 = m[2, 0] + m[2, 1] + m[2, 2];
+
+            int sc1 = m[0, 0] + m[1, 0] + m[2, 0];
+            int sc2 = m[0, 1] + m[1, 1] + m[2, 1];
+            int sc3 = m[0, 2] + m[1, 2] + m[2, 2];
+
+            int sd1 = m[0, 0] + m[1, 1] + m[2, 2];
+            int sd2 = m[0, 2] + m[1, 1] + m[2, 0];
+
+            if (sl1 == sl2 && sl1 == sl3 && sl1 == sc1 && sl1 == sc2 && sl1 == sc3 && sl1 == sd1 && sl1 == sd2)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void view()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(m[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void permutari_pmag(int k, int n, int[] sol, bool[] vis)
+        {
+            if (k >= n)
+            {
+                m[0, 0] = sol[0];
+                m[0, 1] = sol[1];
+                m[0, 2] = sol[2];
+                m[1, 0] = sol[3];
+                m[1, 1] = sol[4];
+                m[1, 2] = sol[5];
+                m[2, 0] = sol[6];
+                m[2, 1] = sol[7];
+                m[2, 2] = sol[8];
+                if (verify())
+                {
+                    view();
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (!vis[i])
+                    {
+                        vis[i] = true;
+                        sol[k] = i + 1;
+                        permutari_pmag(k + 1, n, sol, vis);
+                        vis[i] = false;
+                    }
                 }
             }
         }
@@ -195,7 +306,7 @@ namespace Curs7
             //combinari(0, n, p, v, vis);
             int[] v = new int[n + 1];
             v[0] = 0;
-            combinari1(1, n, p, v);
+            //combinari1(1, n, p, v);
         }
     }
 }
