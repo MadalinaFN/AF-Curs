6d6e384e13be008718c5fa8874b163f8f593838a
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-04-20 08:29:56 +0300

    Queue

---
+++ Curs8/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs8
+{
+    public class Queue
+    {
+        public int[] v;
+        public int n;
+        
+        public Queue()
+        {
+            n = 0;
+        }
+
+        public void push(int value)
+        {
+            n++;
+            int[] t = new int[n];
+            for (int i = 0; i < n - 1; i++)
+            {
+                t[i + 1] = v[i];
+            }
+            t[0] = value;
+            v = t;
+        }
+
+        public int pop()
+        {
+            int tor = v[n - 1];
+            n--;
+            int[] t = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                t[i] = v[i];
+            }
+            v = t;
+            return tor;
+        }
+
+        public void view()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(v[i] + " ");
+            }
+        }
+
+        public void viewLine()
+        {
+            view();
+            Console.WriteLine();
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue A = new Queue();
+            A.push(1);
+            A.push(2);
+            A.push(3);
+            A.push(4);
+            A.view();
+            int t = A.pop();
+            A.view();
+            Console.WriteLine($"Pop = {t}");
+        }
+    }
+}
